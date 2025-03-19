bde99891f2687205839665ce789f397c813d9f81
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-03-19 01:07:49 +0100

    Update after first lesson

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("You are awesome!")
+                .font(.largeTitle)
+                .fontWeight(.thin)
+                .foregroundStyle(.blue)
+
+            HStack {
+                Image(systemName: "figure.australian.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.purple)
+                Image(systemName: "swift")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.orange)
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.green)
+            }
         }
+        // New Stuff Here
         .padding()
     }
 }
@@ -22,3 +39,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
